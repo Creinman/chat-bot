976a9ad1a36c14dc235a4de7c52e6101b89f6dfc
Creinman <68871139+Creinman@users.noreply.github.com>
2020-10-14 23:21:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import zmagi
 import config
 import telebot
 
-
 bot = telebot.TeleBot(config.token)
-
 
 @bot.message_handler(content_types=["text"])
 def zmagi_all_messages(message):
@@ -17,7 +15,6 @@ def zmagi_all_messages(message):
 
     message.text = output.replace("тш", "ч")
     bot.send_message(message.chat.id, message.text)
-
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
